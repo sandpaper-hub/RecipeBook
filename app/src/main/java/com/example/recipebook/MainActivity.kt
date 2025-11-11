1e5badbb460dcf2da219f23b5a8cc92dbf180988
Constantine Kim <electro.passion92@gmail.com>
2025-11-11 10:08:57 +0500

    Add OnboardingScreen with start image

--- app/src/main/java/com/example/recipebook/MainActivity.kt
+++ app/src/main/java/com/example/recipebook/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.recipebook.ui.onboardingScreen.OnboardingScreen
 import com.example.recipebook.ui.theme.RecipeBookTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipeBookTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                OnboardingScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecipeBookTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/recipebook/ui/onboardingScreen/OnboardingScreen.kt
+++ app/src/main/java/com/example/recipebook/ui/onboardingScreen/OnboardingScreen.kt
@@ -1,9 +1,25 @@
 package com.example.recipebook.ui.onboardingScreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.recipebook.R
 
 @Composable
 @Suppress("FunctionName")
 fun OnboardingScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
+        Image(
+            painter = painterResource(id = R.drawable.onboarding_image),
+            contentDescription = "onboarding image",
+            modifier = Modifier.padding(innerPadding),
+            contentScale = ContentScale.FillBounds
+        )
+    }
 }
