4def34605f5a73e44ab9ffd0ddcbcc9dedafc345
Constantine Kim <electro.passion92@gmail.com>
2026-03-11 18:04:28 +0500

    Remove Tabs from RecipesScreen

--- app/src/main/java/com/example/recipebook/presentation/ui/recipesScreen/RecipesScreen.kt
+++ app/src/main/java/com/example/recipebook/presentation/ui/recipesScreen/RecipesScreen.kt
@@ -1,48 +1,39 @@
 package com.example.recipebook.presentation.ui.recipesScreen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.example.recipebook.R
+import com.example.recipebook.domain.model.recipe.getRecipe.RecipeCategory
 import com.example.recipebook.presentation.ui.commonUi.HeadingTextLarge
-import com.example.recipebook.presentation.ui.commonUi.Tabs
-import com.example.recipebook.presentation.ui.recipesScreen.tabs.DraftTab
-import com.example.recipebook.presentation.ui.recipesScreen.tabs.RecipesTab
+import com.example.recipebook.presentation.ui.commonUi.recipe.RecipeCardList
+import com.example.recipebook.presentation.util.toUpdatedAgoText
+import com.example.recipebook.presentation.viewModel.recipesScreen.RecipesViewModel
 
 @Composable
 @Suppress("FunctionName")
-fun RecipesScreen(onRecipeDetail: (recipeId: String) -> Unit) {
-
-    val draftListState = rememberLazyListState()
-    val recipeListState = rememberLazyListState()
-    var selectedTab by rememberSaveable { mutableIntStateOf(0) }
-
-    val tabsTitle = listOf(
-        stringResource(R.string.my_recipes),
-        stringResource(R.string.draft)
-    )
-
-    val tabContents: List<@Composable () -> Unit> = listOf(
-        {
-            RecipesTab(
-                listState = recipeListState,
-                onRecipeDetail = onRecipeDetail
-            )
-        },
-        { DraftTab(draftListState) }
-    )
+fun RecipesScreen(
+    viewModel: RecipesViewModel = hiltViewModel(),
+    onRecipeDetail: (recipeId: String) -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsState()
 
     ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-        val (headingText, contentTab) = createRefs()
+        val (headingText, contentContainer) = createRefs()
+        val startGuideline = createGuidelineFromStart(24.dp)
+        val endGuideline = createGuidelineFromEnd(24.dp)
 
         HeadingTextLarge(
             text = stringResource(R.string.recipes),
@@ -53,17 +44,41 @@ fun RecipesScreen(onRecipeDetail: (recipeId: String) -> Unit) {
                 }
         )
 
-        Tabs(
-            titles = tabsTitle,
-            tabContents = tabContents,
-            onTabSelected = { selectedTab = it },
-            selectedTab = selectedTab,
+        LazyColumn(
             modifier = Modifier
-                .constrainAs(contentTab) {
-                    linkTo(start = parent.start, end = parent.end)
-                    top.linkTo(headingText.bottom, margin = 17.dp)
-                    bottom.linkTo(parent.bottom)
+                .fillMaxWidth()
+                .constrainAs(contentContainer) {
+                    linkTo(start = startGuideline, end = endGuideline,
+                        top = headingText.bottom, bottom = parent.bottom,
+                        topMargin = 24.dp)
                     height = Dimension.fillToConstraints
-                })
+                },
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+        ) {
+            items(
+                items = uiState.newRecipes,
+                key = { it.id }) { recipe ->
+                RecipeCardList(
+                    recipeId = recipe.id,
+                    imageUrl = recipe.imageUrl,
+                    categoryResource = when (recipe.category) {
+                        RecipeCategory.APPETIZER -> R.string.appetizer
+                        RecipeCategory.SALAD -> R.string.salad
+                        RecipeCategory.SOUP -> R.string.soup
+                        RecipeCategory.MAIN -> R.string.main
+                        RecipeCategory.GARNISH -> R.string.garnish
+                        RecipeCategory.SAUCE -> R.string.sauce
+                        RecipeCategory.DESERT -> R.string.desert
+                        RecipeCategory.DRINK -> R.string.drink
+                        else -> R.string.unknown_measure
+                    },
+                    name = recipe.recipeName,
+                    timeEstimation = recipe.recipeTimeEstimation,
+                    uploadedTime = recipe.createdAt.toUpdatedAgoText(),
+                    onRecipeClick = { onRecipeDetail(recipe.id) },
+                    modifier = Modifier.padding(horizontal = 24.dp)
+                )
+            }
+        }
     }
 }
