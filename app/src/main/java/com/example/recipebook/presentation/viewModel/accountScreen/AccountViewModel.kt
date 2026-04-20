7596d10b913b17715986ff7fa5b9be2e7594da92
Constantine Kim <electro.passion92@gmail.com>
2026-04-20 12:05:59 +0500

    Refactor unused import

--- app/src/main/java/com/example/recipebook/presentation/viewModel/accountScreen/AccountViewModel.kt
+++ app/src/main/java/com/example/recipebook/presentation/viewModel/accountScreen/AccountViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.recipebook.presentation.viewModel.accountScreen
 
 import android.net.Uri
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.recipebook.domain.Constraints
@@ -18,7 +17,6 @@ import com.example.recipebook.presentation.viewModel.accountScreen.model.Account
 import com.example.recipebook.presentation.viewModel.model.FormField
 import com.example.recipebook.presentation.viewModel.model.ImageSource
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
